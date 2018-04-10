eae15adacf8664261cd4ee597311a509298b336a
Jude Beadle <info@jbeadle.de>
2018-04-10 13:58:49 +0200

    cleaning up code

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ let timer = null;
 let seconds = 0;
 let moves = 0;
 
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle() {
     // remove all li.cards from the DOM
@@ -123,7 +122,6 @@ function displayMoves(){
 	}
 }
 
-
 function gameSummary(){
 	container.prepend(summaryPanel);
 	summaryPanel.className = 'summary-panel';
@@ -155,24 +153,7 @@ function endGame() {
 	container.classList.add('end');
 }
 
-
-
 //starting up
 newGame();
 deck.addEventListener('click', showcard);
 restartButton.addEventListener('click', newGame);
-
-
-
-
-
-
-/*
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
